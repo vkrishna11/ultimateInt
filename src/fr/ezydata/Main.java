f52ac12af2ce5894ab62504c526a08098f7c4238
Vamshi Devraj <vkrishna11@gmail.com>
2020-05-09 15:31:21 +0200

    Initial commit

---
+++ src/fr/ezydata/Main.java
@@ -0,0 +1,11 @@
+package fr.ezydata;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        // Hello, these lines were added
+        // can you see it
+
+    }
+}
